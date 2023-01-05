aee4b963c6bd25cd0a830d5bc7a108d32fb54ca6
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2023-01-05 20:43:06 +0530

    Added basic ui(Navbar and form)

---
+++ client/src/components/SelectBox.js
@@ -0,0 +1,9 @@
+import Select from 'react-select'
+
+export default function SelectBox(props) {
+    return (
+        <div className="w-48">
+            <Select options={props.options} />
+        </div>
+    )
+}
