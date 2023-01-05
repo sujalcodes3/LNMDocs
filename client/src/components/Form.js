ef20bf9dd28b042e3d770ea39e1a5069dce4c548
Sujal Jain <ethicalwolf333@gmail.com>
2023-01-05 23:29:51 +0530

    created options.js and used tailwind react select box

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,13 +1,14 @@
 import SelectBox from "./SelectBox";
 import { Button } from "@material-tailwind/react";
+import * as options from "../data/options.js";
 
 export default function FormSelect(props) {
   return (
     <div className='h-screen flex flex-col justify-center items-center gap-y-10'>
       <div className='h-max w-screen flex justify-evenly items-center'>
-        <SelectBox name='Subject' />
-        <SelectBox name='Type' />
-        <SelectBox name='Year' />
+        <SelectBox name='Subject' options={options.subjects} />
+        <SelectBox name='Type' options={options.types} />
+        <SelectBox name='Year' options={options.years} />
       </div>
       <Button variant='outlined' className='w-48 m-auto my-4 '>
         Search
--- client/src/data/options.js
+++ client/src/data/options.js
@@ -1,5 +1,13 @@
-export const subjects = ["COA", "M3"];
+import { Option } from "@material-tailwind/react";
 
-export const types = ["Notes", "Previous-Year Papers"];
+export const subjects = ["COA", "M3"].map((ele) => {
+  return <Option>{ele}</Option>;
+});
 
-export const years = ["2018", "2019", "2020", "2021"];
+export const types = ["Notes", "Previous-Year Papers"].map((ele) => {
+  return <Option>{ele}</Option>;
+});
+
+export const years = ["2018", "2019", "2020", "2021"].map((ele) => {
+  return <Option>{ele}</Option>;
+});
