2f151cec3bc2a8d90c6f17646b3cbab6492c09db
Sujal Jain <ethicalwolf333@gmail.com>
2023-01-05 23:23:01 +0530

    dummy commit

--- client/src/components/SelectBox.js
+++ client/src/components/SelectBox.js
@@ -1,12 +1,9 @@
 import Select from "react-select";
 
 export default function SelectBox(props) {
-  const selectedHandler = (selectedOption) => {
-    console.log(selectedOption.value);
-  };
   return (
     <div className='w-48'>
-      <Select onChange={selectedHandler} options={props.options} />
+      <Select options={props.options} />
     </div>
   );
 }
---
+++ client/src/data/options.js
@@ -0,0 +1,5 @@
+export const subjects = ["COA", "M3"];
+
+export const types = ["Notes", "Previous-Year Papers"];
+
+export const years = ["2018", "2019", "2020", "2021"];
