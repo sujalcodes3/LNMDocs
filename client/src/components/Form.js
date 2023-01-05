c9366482e41c989c4ade932e91ddebf9b8d94809
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2023-01-06 00:02:17 +0530

    Completed Select menu and Button

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,16 +1,46 @@
-import SelectBox from "./SelectBox";
-import { Button } from "@material-tailwind/react";
-import * as options from "../data/options.js";
+import { useState } from "react";
+import { Button, Select, Option } from "@material-tailwind/react";
 
 export default function FormSelect(props) {
+  const [value, setValue] = useState({
+    subject: "",
+    type: "",
+    year: "",
+  });
+  const subjectChangeHandler = (value) => {
+    console.log(value);
+  }
+  const typeChangeHandler = (value) => {
+    console.log(value);
+  }
+  const yearChangeHandler = (value) => {
+    console.log(value);
+  }
+  const subjects = ["COA", "M3"].map((ele) => {
+    return <Option value={ele}>{ele}</Option>;
+  });
+
+  const types = ["Notes", "Previous-Year Papers"].map((ele) => {
+    return <Option value={ele}>{ele}</Option>;
+  });
+
+  const years = ["2018", "2019", "2020", "2021"].map((ele) => {
+    return <Option value={ele}>{ele}</Option>;
+  });
+  const animation =
+  {
+    mount: { y: 0 },
+    unmount: { y: 25 },
+  }
+
   return (
     <div className='h-screen flex flex-col justify-center items-center gap-y-10'>
-      <div className='h-max w-screen flex justify-evenly items-center'>
-        <SelectBox name='Subject' options={options.subjects} />
-        <SelectBox name='Type' options={options.types} />
-        <SelectBox name='Year' options={options.years} />
+      <div className='h-max w-max flex justify-evenly items-center gap-10'>
+        <Select color="orange" label="Select Subject" animate={animation} onChange={subjectChangeHandler}>{subjects}</Select>
+        <Select animate={animation} color="yellow" label="Select Type" onChange={typeChangeHandler}>{types}</Select>
+        <Select animate={animation} color="green" label="Select Year" onChange={yearChangeHandler}>{years}</Select>
       </div>
-      <Button variant='outlined' className='w-48 m-auto my-4 '>
+      <Button variant='filled' className='w-48 m-auto my-4' disabled>
         Search
       </Button>
     </div>
--- client/src/components/SelectBox.js
+++
@@ -1,9 +0,0 @@
-import Select from "@material-tailwind/react";
-
-export default function SelectBox(props) {
-  return (
-    <div className='w-48'>
-      <Select label={`"Select "  ${props.name}`}>{props.options}</Select>
-    </div>
-  );
-}
