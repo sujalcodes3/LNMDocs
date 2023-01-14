b35b138cff85191749e1f1b379d75cfcf6f25824
Sujal Jain <ethicalwolf333@gmail.com>
2023-01-14 20:24:35 +0530

    trying to make a custom hook

---
+++ client/src/hooks/useHttp.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+const useHttp = (applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+};
+
+export default useHttp;
