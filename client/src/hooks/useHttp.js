e4936a080290acf894677c616c06f77b9a3c861c
Sujal Jain <ethicalwolf333@gmail.com>
2023-01-20 11:41:14 +0530

    Syncing the branch with main

--- client/src/hooks/useHttp.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-const useHttp = (applyData) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-};
-
-export default useHttp;
